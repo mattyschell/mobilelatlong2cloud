f877375c5e586d22b0e32dbc71d27e6e9ee14d27
Matt Schell <mschell@doitt.nyc.gov>
2024-06-27 10:24:04 -0400

    corrects test sql aliasing

--- test/citylimits.sql
+++ test/citylimits.sql
@@ -1,7 +1,7 @@
 with gishq as (
-select ST_SETSRID(ST_GeomFromText('POINT(988208 191860)'),2263)
+select ST_SETSRID(ST_GeomFromText('POINT(988208 191860)'),2263) as geom
 ), nullisland as (
-select ST_SETSRID(ST_GeomFromText('POINT(-1 -1)'),2263)
+select ST_SETSRID(ST_GeomFromText('POINT(0 0)'),2263) as geom
 )
 SELECT 
     ST_Contains(c.geom, (select geom from gishq))  
