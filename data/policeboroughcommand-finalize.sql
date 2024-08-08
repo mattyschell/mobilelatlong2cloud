1f7e29caefc85a653dbf9295e5fcf1f0441167e0
Matt Schell <mschell@doitt.nyc.gov>
2024-08-08 12:46:33 -0400

    st_multis policeboroughcommand resolves #15

--- data/policeboroughcommand-finalize.sql
+++ data/policeboroughcommand-finalize.sql
@@ -2,18 +2,20 @@ insert into
     geo_districts (geom
                   ,layer_name
                   ,feature_value)
-select
-    ST_Union(ARRAY(select 
-                       geom 
-                   from 
-                       geo_districts
-                   where 
-                       layer_name = 'POLICEPRECINCT'
-                   and 
-                       feature_value 
-                   in ('001','005','006','007','009','010','013','014','017','018')
-                   )
-            )
+select 
+    ST_MULTI(
+        ST_Union(ARRAY(select 
+                        geom 
+                    from 
+                        geo_districts
+                    where 
+                        layer_name = 'POLICEPRECINCT'
+                    and 
+                        feature_value 
+                    in ('001','005','006','007','009','010','013','014','017','018')
+                    )
+                )
+        )
     ,'POLICEBOROUGHCOMMAND'
     ,'Manhattan South';
 insert into 
@@ -21,17 +23,19 @@ insert into
                   ,layer_name
                   ,feature_value)
 select
-    ST_Union(ARRAY(select 
-                       geom 
-                   from 
-                       geo_districts
-                   where 
-                       layer_name = 'POLICEPRECINCT'
-                   and 
-                       feature_value 
-                   in ('019','020','022','023','024','025','026','028','030','032','033','034')
-                   )
-            )
+    ST_MULTI(
+        ST_Union(ARRAY(select 
+                        geom 
+                    from 
+                        geo_districts
+                    where 
+                        layer_name = 'POLICEPRECINCT'
+                    and 
+                        feature_value 
+                    in ('019','020','022','023','024','025','026','028','030','032','033','034')
+                    )
+                )
+        )
     ,'POLICEBOROUGHCOMMAND'
     ,'Manhattan North';
 insert into 
@@ -39,17 +43,19 @@ insert into
                   ,layer_name
                   ,feature_value)
 select
-    ST_Union(ARRAY(select 
-                       geom 
-                   from 
-                       geo_districts
-                   where 
-                       layer_name = 'POLICEPRECINCT'
-                   and 
-                       feature_value 
-                   in ('040','041','042','043','044','045','046','047','048','049','050','052')
-                   )
-            )
+    ST_MULTI(
+        ST_Union(ARRAY(select 
+                        geom 
+                    from 
+                        geo_districts
+                    where 
+                        layer_name = 'POLICEPRECINCT'
+                    and 
+                        feature_value 
+                    in ('040','041','042','043','044','045','046','047','048','049','050','052')
+                    )
+                )
+        )
     ,'POLICEBOROUGHCOMMAND'
     ,'The Bronx';
 insert into 
@@ -57,17 +63,19 @@ insert into
                   ,layer_name
                   ,feature_value)
 select
-    ST_Union(ARRAY(select 
-                       geom 
-                   from 
-                       geo_districts
-                   where 
-                       layer_name = 'POLICEPRECINCT'
-                   and 
-                       feature_value 
-                   in ('060','061','062','063','066','067','068','069','070','071','072','076','078')
-                   )
-            )
+    ST_MULTI(
+        ST_Union(ARRAY(select 
+                        geom 
+                    from 
+                        geo_districts
+                    where 
+                        layer_name = 'POLICEPRECINCT'
+                    and 
+                        feature_value 
+                    in ('060','061','062','063','066','067','068','069','070','071','072','076','078')
+                    )
+                )
+        )
     ,'POLICEBOROUGHCOMMAND'
     ,'Brooklyn South';
 insert into 
@@ -75,17 +83,19 @@ insert into
                   ,layer_name
                   ,feature_value)
 select
-    ST_Union(ARRAY(select 
-                       geom 
-                   from 
-                       geo_districts
-                   where 
-                       layer_name = 'POLICEPRECINCT'
-                   and 
-                       feature_value 
-                   in ('073','075','077','079','081','083','084','088','090','094')
-                   )
-            )
+    ST_MULTI(
+        ST_Union(ARRAY(select 
+                        geom 
+                    from 
+                        geo_districts
+                    where 
+                        layer_name = 'POLICEPRECINCT'
+                    and 
+                        feature_value 
+                    in ('073','075','077','079','081','083','084','088','090','094')
+                    )
+                )
+        )
     ,'POLICEBOROUGHCOMMAND'
     ,'Brooklyn North';
 insert into 
@@ -93,17 +103,19 @@ insert into
                   ,layer_name
                   ,feature_value)
 select
-    ST_Union(ARRAY(select 
-                       geom 
-                   from 
-                       geo_districts
-                   where 
-                       layer_name = 'POLICEPRECINCT'
-                   and 
-                       feature_value 
-                   in ('100','101','102','103','105','106','107','113')
-                   )
-            )
+    ST_MULTI(
+        ST_Union(ARRAY(select 
+                        geom 
+                    from 
+                        geo_districts
+                    where 
+                        layer_name = 'POLICEPRECINCT'
+                    and 
+                        feature_value 
+                    in ('100','101','102','103','105','106','107','113')
+                    )
+                )
+        )
     ,'POLICEBOROUGHCOMMAND'
     ,'Queens South';
 insert into 
@@ -111,17 +123,19 @@ insert into
                   ,layer_name
                   ,feature_value)
 select
-    ST_Union(ARRAY(select 
-                       geom 
-                   from 
-                       geo_districts
-                   where 
-                       layer_name = 'POLICEPRECINCT'
-                   and 
-                       feature_value 
-                   in ('104','108','109','110','111','112','114','115')
-                   )
-            )
+    ST_MULTI(
+        ST_Union(ARRAY(select 
+                        geom 
+                    from 
+                        geo_districts
+                    where 
+                        layer_name = 'POLICEPRECINCT'
+                    and 
+                        feature_value 
+                    in ('104','108','109','110','111','112','114','115')
+                    )
+                )
+        )
     ,'POLICEBOROUGHCOMMAND'
     ,'Queens North';
 insert into 
@@ -129,17 +143,19 @@ insert into
                   ,layer_name
                   ,feature_value)
 select
-    ST_Union(ARRAY(select 
-                       geom 
-                   from 
-                       geo_districts
-                   where 
-                       layer_name = 'POLICEPRECINCT'
-                   and 
-                       feature_value 
-                   in ('120','121','122','123')
-                   )
-            )
+    ST_MULTI(
+        ST_Union(ARRAY(select 
+                        geom 
+                    from 
+                        geo_districts
+                    where 
+                        layer_name = 'POLICEPRECINCT'
+                    and 
+                        feature_value 
+                    in ('120','121','122','123')
+                    )
+                )
+        )
     ,'POLICEBOROUGHCOMMAND'
     ,'Staten Island';
 -- remove interior ring artifact slivers
@@ -172,7 +188,7 @@ as (
 update 
     geo_districts 
 set 
-    geom = pbcexteriorring.geom
+    geom = st_multi(pbcexteriorring.geom)
 from 
     pbcexteriorring
 where
@@ -181,11 +197,14 @@ where
 update 
     geo_districts
 set 
-    geom = st_buffer(st_buffer(geom
-                              ,.1
-                              ,'join=mitre')
-                    ,-.1
-                    ,'join=mitre')
+    geom = st_multi(st_buffer(st_buffer(geom
+                                        ,.1
+                                        ,'join=mitre'
+                                        )
+                            ,-.1
+                            ,'join=mitre'
+                            )
+                    )
 where 
     layer_name = 'POLICEBOROUGHCOMMAND'
 and feature_value in ('Brooklyn South'
